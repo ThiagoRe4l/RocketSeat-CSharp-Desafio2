42a76948d9c80f7717bdf9333be41c3635eeb041
Thiago Luiz <tl7thiago@gmail.com>
2025-06-11 20:34:45 -0300

    teste

---
+++ Petfolio/Petfolio.Application/UseCases/Pets/GetById/GetByIdUseCase.cs
@@ -0,0 +1,17 @@
+﻿using Petfolio.Communication.Responses;
+
+namespace Petfolio.Application.UseCases.Pets.GetById;
+
+public class GetByIdUseCase
+{
+    public ResponsePetJson Execute(int id)
+    {
+        return new ResponsePetJson
+        {
+            Id = id,
+            Name = "Fido",
+            Birthday = new DateTime(year: 2023, month: 01, day: 01),
+            Type = Communication.Enums.PetType.Dog
+        };
+    }
+}
